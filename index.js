dca07d99ad7636f9528b98e91179350ec3197b56
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-11-25 18:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const taskRoutes = require("./src/routes/taskRoutes");
-
+require("dotenv").config();
 //middleware
 const app = express();
 app.use(cors());
@@ -10,17 +10,11 @@ app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
 mongoose
-  .connect(
-    "mongodb+srv://officePc:Ewsl7PZcOLC4zFyT@cluster0.wn4j5.mongodb.net/"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 const PORT = 5000;
-
-app.get('/', (req, res)=>{
-  res.send('App is working');
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
