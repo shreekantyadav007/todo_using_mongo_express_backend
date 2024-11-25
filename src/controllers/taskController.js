c6146d729985597f542cc56d0717aa77cee3de34
Shreekant <shreey94@gmail.com>
2024-11-25 15:05:51 +0530

    first commit

---
+++ src/controllers/taskController.js
@@ -0,0 +1,57 @@
+const Task = require('../models/taskModel');
+
+// Create a new task
+exports.createTask = async(req, res) =>{
+    try{
+        const {title, description} = req.body;
+        const task = new Task({title, description});
+        await task.save();
+        res.status(201).json(task);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+// Get all tasks
+exports.getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Get a single task by ID
+exports.getTaskById = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+        res.status(200).json(task);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Update a task
+exports.updateTask = async (req, res) => {
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+        res.status(200).json(task);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Delete a task
+exports.deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+        res.status(200).json({ message: 'Task deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const taskRoutes = require("./routes/taskRoutes");
+
+//middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/tasks", taskRoutes);
+
+mongoose
+  .connect(
+    "mongodb+srv://officePc:Ewsl7PZcOLC4zFyT@cluster0.wn4j5.mongodb.net/"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
