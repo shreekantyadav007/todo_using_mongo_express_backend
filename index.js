e1a04f2dfa67d0b0e829b6fa14d32e2254424947
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-11-26 14:55:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+app.get('/', async (req, res)=>{
+   try {
+        const db = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = db.connections[0].readyState; // Connection state (1 = connected)
+        res.send("Connected to MongoDB");
+         res.end();
+    } catch (error) {
+        res.send("MongoDB connection failed:", error.message);
+        throw error; // Ensure errors are logged
+       res.end();
+    }
+})
 app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
