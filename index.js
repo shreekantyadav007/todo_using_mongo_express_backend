520f1fb27138ec5f3b746374e4d2093675952edf
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-11-26 14:36:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,41 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
-let isConnected; 
 
-async function connectToDatabase() {
-    if (isConnected) {
-        console.log("Using existing MongoDB connection");
-        return;
-    }
-
-    try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        isConnected = db.connections[0].readyState;
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        throw error;
-    }
-}
-
-app.get('/', async (req, res) => {
-    try {
-        await connectToDatabase(); // Ensure MongoDB is connected
-        if (req.method === "GET") {
-            const tasks = await Task.find(); // Fetch tasks
-            return res.status(200).json(tasks);
-        } else {
-            return res.status(405).json({ error: "Method not allowed" });
-        }
-    } catch (error) {
-        console.error("Error in API:", error.message);
-        return res.status(500).json({ error: "Internal server error" });
-    }
-});
 const PORT = 5000;
 
 app.listen(PORT, () => {
