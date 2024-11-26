2c2eeaf5b1f975a790e60682e03be67938c30b85
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-11-26 16:15:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 const mongoose = require("mongoose");
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+const Task =  mongoose.model("Task", taskSchema);
 const express = require("express");
 const cors = require("cors");
-const taskRoutes = require("./src/routes/taskRoutes");
 require("dotenv").config();
 //middleware
 const app = express();
-
-app.get('/', async (req, res)=>{
-   try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-       
-        res.send("Connected to MongoDB");
-         res.end();
-    } catch (error) {
-        res.send("MongoDB connection failed:", error.message);
-        throw error; // Ensure errors are logged
-       res.end();
-    }
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 app.use(cors());
 app.use(express.json());
-app.use("/api/tasks", taskRoutes);
+app.post("/", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const task = new Task({ title, description });
+    await task.save();
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+app.get("/", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+app.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.put("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 
 const PORT = 5000;
 
